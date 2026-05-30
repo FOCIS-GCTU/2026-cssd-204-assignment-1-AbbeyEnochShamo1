0d57e96e8e626990452ce329144a4e6d71c7d4c6
AbbeyEnochShamo1 <2425404607@live.gctu.edu.gh>
2026-05-30 17:57:05 +0000

    Add files via upload
    Scripting Languages Assignment 2
    Name: Enoch Shamo Abbey
    Index Number: 2425404607
    Programme: Cyber Security Group B

---
+++ ESA.py
@@ -0,0 +1,30 @@
+#File: ESA.py
+#Description: Print out my initials in large stylized letters
+#Assignment 1
+
+    #Name: Enoch Shamo Abbey
+    #Index No: 2425404607
+    #Email : 2425404607@live.gctu.edu.gh
+    #Grader: Augustus Buckman
+    #
+    # On my honor,Enoch Shamo Abbey, this programming assignment is my own work
+    #and I have not provided this code to any other student.
+
+def main():
+    # Print my initials in stylized large letters
+
+    print()
+    print("...ESA")
+    print()
+
+    print("...EEEEEEEEEEEE..........SSSSSSSSSS............AAA..........")
+    print("...EEE..................SSSSSSSSSSS..........AA...AA........")
+    print("...EEE.................SSSS.................AA.....AA.......")
+    print("...EEE.................SSSS.................AA.....AA.......")
+    print("...EEE...................SSSSSS.............AA.....AA.......")
+    print("...EEEEEEEEEEEE............SSSSSS...........AAAAAAAAA.......")
+    print("...EEE.........................SSSS.........AA.....AA.......")
+    print("...EEE.........................SSSS.........AA.....AA.......")
+    print("...EEE............**...SSSSSSSSSSS....**....AA.....AA.....**")
+    print("...EEEEEEEEEEEE...**...SSSSSSSSSSS....**....AA.....AA.....**")
+main()
